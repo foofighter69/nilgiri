9f07c1ef37ff216aa60131377d245c2690aa22a5
foofighter69 <foofighter69@0a1c11c0-7440-64b2-5844-390a96cf8d9c>
2010-10-05 14:17:17 +0000

    new selection algorithm=random

--- test_ring.lua
+++ test_ring.lua
@@ -73,4 +73,87 @@ function test_addandremove()
 	assert_equal("m", r:next())
 	assert_equal("b", r:next())
 end
+function test_random_firstnumber()
+	
+	r:setSelectAlgorithm("random")
+	assert_not_nil(r:next())
+	
+end
+function test_random_notnil()
+	r:setSelectAlgorithm("random")
+	
+	for i=1,8 do
+	assert_not_nil(r:next())
+	end
+end
+function test_random_repeatcheck_innercycle()
+	r:setSelectAlgorithm("random")
+	
+	--local test_table = {[1] = 1, [2] = 2, [3] = 3, [4] = 4, [5] = 5, [6] = 6, [7] = 7 }
+	--local test_table = {[1] = "e", [2] = "c", [3] = "a", [4] = "d", [5] = "b", [6] = "g", [7] = "f" }
+	--local test_table = {[1]="a",[2]="b",[3] = "c",[4] ="d",[5]="e",[6]="f",[7]="g"}
+	
 
+	--r:addTable(test_table)
+	function c()
+		
+		local t_i = {}
+		for i=1,7 do 
+			entry = r:next()
+			
+			for _,v in ipairs(t_i) do
+				
+				assert_not_equal(v, entry, "[-] v: ".. v..", e: "..entry) --..#r._t_randomshadow)
+				
+			end
+			--io.write(entry)
+			table.insert(t_i, entry)
+		end
+
+	end
+	for s=1,7 do
+	
+		c()
+		--print("---")
+		
+	end
+end	
+function test_random_remove()
+	r:setSelectAlgorithm("random")
+
+		
+	function tst()
+		assert_not_nil(r:next())
+		r:remove()
+		--print(r)
+	end
+	for i=1,7 do
+	tst()
+	end
+	local test_table = {[1]="a",[2]="b",[3] = "c",[4] ="d",[5]="e",[6]="f",[7]="g"}
+	
+
+	r:addTable(test_table)	
+
+	
+end
+function test_random_removeinsert()
+	r:setSelectAlgorithm("random")
+	
+	r:next()
+	print(r)
+	
+	r:next()
+	print(r)
+	
+	r:remove()
+	print(r)
+	
+	r:add("u")
+	r:next()
+	
+	print(r)
+	
+	
+end
+	
